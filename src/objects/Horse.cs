a20a7d8a1fb16c356f2e6c9ff908d08b9a417ab2
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-12 17:12:31 +0100

    Created class for a horse with properties such as name, d.o.b. and ID.

---
+++ src/objects/Horse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horse_Race_App.src.objects
+{
+    internal class Horse
+    {
+        public string horseName { get; set; }
+        public DateTime birthDate { get; set; } //when creating (yyyy, mm, dd)
+        public string horseID { get; set; }
+
+        public Horse(string name, DateTime date, string horse) 
+        {
+            horseName = name;
+            birthDate = date;
+            horse = horseID;
+            // this is gonna be the pattern for the ID of a horse - @"\b[A-Z]{3}\d{9}\b"
+        }
+    }
+}
