64a10096464c99a3ee109971e508e7e7efd73847
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-13 15:13:11 +0100

    Created a class for Racegoer.

---
+++ src/people/Racegoer.cs
@@ -0,0 +1,21 @@
+﻿using Horse_Race_App.src.objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horse_Race_App.src.people
+{
+    internal class Racegoer
+    {
+        public void ViewUpcomingEvents(List<RaceEvent> events)
+        {
+            Console.WriteLine("Upcoming Events:");
+            foreach (var raceEvent in events)
+            {
+                Console.WriteLine(raceEvent);
+            }
+        }
+    }
+}
