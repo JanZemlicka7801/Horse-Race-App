3e727b172f186c7c3ff8cedc98d6f7cef696076b
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-13 18:20:07 +0100

    Fixed up display of events.

--- src/people/Racegoer.cs
+++ src/people/Racegoer.cs
@@ -7,14 +7,22 @@ using System.Threading.Tasks;
 
 namespace Horse_Race_App.src.people
 {
-    internal class Racegoer
+    public class Racegoer
     {
-        public static void ViewUpcomingEvents(List<RaceEvent> events)
+        public static void ViewUpcomingEvents(List<RaceEvents> events)
         {
             Console.WriteLine("Upcoming Events:");
             foreach (var raceEvent in events)
             {
                 Console.WriteLine(raceEvent);
+                foreach (var race in raceEvent.Races)
+                {
+                    Console.WriteLine($"  {race}");
+                    foreach (var horse in race.Horses)
+                    {
+                        Console.WriteLine($"    {horse}");
+                    }
+                }
             }
         }
     }
