5a1a5594f088bb52e14014902bd2bdd2b3190dab
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-13 15:19:58 +0100

    Class for horse owner where can user insert a horse inside the race.

---
+++ src/people/HorseOwner.cs
@@ -0,0 +1,18 @@
+﻿using Horse_Race_App.src.objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horse_Race_App.src.people
+{
+    internal class HorseOwner
+    {
+        public void EnterHorseInRace(Race race, string name, DateTime date, string horse)
+        {
+            race.AddHorse(new Horse(name, date, horse));
+            Console.WriteLine($"{horse.Name} has been entered in {race.Name}.");
+        }
+    }
+}
