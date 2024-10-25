a8a950567bfea470851f450c78a23912cfbc5bd6
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-25 13:24:34 +0100

    Added method to remove the horse from the race and also to add the horse inside the race.

--- src/people/HorseOwner.cs
+++ src/people/HorseOwner.cs
@@ -7,8 +7,31 @@ namespace Horse_Race_App.people
         public void EnterHorseInRace(Race race, string name, DateTime date, string horseId)
         {
             Horse horse = new Horse(name, date, horseId);
-            race.AddHorse(horse);
-            Console.WriteLine($"{horse.HorseName} has been entered in {race.Name}.");
+            Console.WriteLine(race.AddHorse(horse)
+                ? $"{horse.HorseName} has been entered in {race.Name}."
+                : "Horse has not been entered.");
+        }
+
+        public void RemoveHorseFromRace(Race race, string name)
+        {
+            Horse horseToRemove = race.Horses.Where(h => h.HorseName == name).FirstOrDefault();
+
+            if (horseToRemove != null)
+            {
+                if (race.RemoveHorse(horseToRemove))
+                {
+                    Console.WriteLine($"Horse {name} has been removed from {race.Name}.");
+                    race.RemoveHorse(horseToRemove);
+                }
+                else
+                {
+                    Console.WriteLine($"Horse {name} has not been removed from {race.Name}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Horse {name} is not in the {race.Name} race.");
+            }
         }
     }
 }
